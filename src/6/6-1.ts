b0c52a8110908ced1238a3c38a879ca440489702
nbabcock <nathan.r.babcock@gmail.com>
2021-12-06 12:53:01 -0600

    6-1

--- src/6/6-1.ts
+++ src/6/6-1.ts
@@ -1,9 +1,26 @@
 import { readFileSync } from 'fs'
 
-const raw = readFileSync('src/6/test.txt', 'utf8')
-const lines = raw.split(/\r?\n/)
+const raw = readFileSync('src/6/input.txt', 'utf8')
+const fish = raw.split(',').map(Number)
 
-console.log('Lines', lines.length)
-console.log('Lines', lines)
+console.log('Fish', fish.length)
+// console.log('Fish', fish)
 
-//
+function simulate(fish: number[], days: number): number {
+  for (let i = 0; i < days; i++) {
+    const newFish: number[] = []
+    for (let j = 0; j < fish.length; j++) {
+      if (fish[j] === 0) {
+        fish[j] = 6
+        newFish.push(8)
+      } else 
+        fish[j]--
+    }
+    fish.push(...newFish)
+  }
+  return fish.length
+}
+
+// console.log('After 18 days', simulate(fish, 18))
+// console.log('After 80 days', simulate(fish, 80-18))
+console.log('After 80 days', simulate(fish, 80))
